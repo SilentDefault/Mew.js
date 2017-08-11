4319ff71bf6e7ec393e4bdf4bbe08ee2a7692efe
SilentDefault <silentdefault@gmail.com>
2017-08-11 10:38:11 -0500

    Update mew.js

--- mew.js
+++ mew.js
@@ -76,7 +76,7 @@
                 }
             },
             en: {
-                month: { 0: 'January', 1: 'February', 2: 'March', 3: 'April', 4: 'May', 5: 'June', 6: 'July', 7: 'August', 8: 'September', 9: 'October', 10: 'November', 11: 'Dicember' },
+                month: { 0: 'January', 1: 'February', 2: 'March', 3: 'April', 4: 'May', 5: 'June', 6: 'July', 7: 'August', 8: 'September', 9: 'October', 10: 'November', 11: 'December' },
                 day: { 0: 'Sunday', 1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday' },
                 ordinal: { 1: 'First', 2: 'Second', 3: 'Third', 4: '' },
                 cardinal: {
